eaa7c13c7732dcc4bb85cc15d53c1b786957b75b
Kenold Beauplan <bkenold@gmail.com>
2017-07-26 20:59:28 -0400

    change wording

--- custom.js
+++ custom.js
@@ -145,7 +145,7 @@ function calculateTotal() {
     //display the result
     var divobj = document.getElementById('totalPrice');
     divobj.style.display = 'block';
-    divobj.innerHTML = "Total Price For this print job: $" + printPrice;
+    divobj.innerHTML = "Total Pricing Quote: $" + printPrice;
 
 }
 
